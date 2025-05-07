9df15cde7dd1985d9a07773431edac13b51c8b5b
Leonardo Lupchinski <lupchinskileonardo@gmail.com>
2025-05-07 14:38:06 +0100

    Fixed some fly things

--- MacroAnalysis/app.py
+++ MacroAnalysis/app.py
@@ -781,5 +781,4 @@ def format_number(value):
     return str(value)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=True)
